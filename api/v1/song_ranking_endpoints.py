9f5948e52efb2f76c6cf804a38efe6c33fc24c3e
Nacho Pinero <piwero@gmail.com>
2023-03-02 22:22:00 +0000

    Add swagger schemas

--- api/v1/song_ranking_endpoints.py
+++ api/v1/song_ranking_endpoints.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import (
     exceptions,
     serializers,
@@ -41,10 +42,29 @@ class SongRankingViewSet(APIView):
         else:
             return NonAuthenticatedSongSerializer
 
+    @extend_schema(
+        responses={
+            200: (
+                AuthenticatedSongSerializer(many=True),
+                "List of songs data for auth users.",
+            ),
+            200: (
+                NonAuthenticatedSongSerializer(many=True),
+                "List of songs data for non auth users",
+            ),
+        }
+    )
     def get(self, request, *args, **kwargs):
         serializer = self.get_serializer_class()(self.get_queryset(), many=True)
         return Response(serializer.data)
 
+    @extend_schema(
+        request=AuthenticatedSongSerializer,
+        responses={
+            201: AuthenticatedSongSerializer,
+            400: "Non authenticated users cannot create songs.",
+        },
+    )
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer_class()(data=request.data)
         serializer.is_valid(raise_exception=True)
